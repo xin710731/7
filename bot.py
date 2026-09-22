f24f3ab15693a82c8e3d7195f049358444c008d1
xin710731 <xin710731@gmail.com>
2026-09-22 16:06:25 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -41,6 +41,7 @@ FAQ = {
 def main_keyboard() -> InlineKeyboardMarkup:
     return InlineKeyboardMarkup([
         [InlineKeyboardButton("🏠 About 658pay", callback_data="about"), InlineKeyboardButton("💰 How To Earn", callback_data="earn")],
+        [InlineKeyboardButton("📋 View Task Details", url="https://bittaro.io"), InlineKeyboardButton("📢 Join TG Channel", url="https://t.me/DNYXW006"),],
         [InlineKeyboardButton("💳 Deposit Issues", callback_data="deposit_menu"), InlineKeyboardButton("💸 Withdrawal Issues", callback_data="withdraw_menu")],
         [InlineKeyboardButton("👥 Agent Commission", callback_data="agent_menu"), InlineKeyboardButton("💬 Contact Support", callback_data="contact")],
     ])
